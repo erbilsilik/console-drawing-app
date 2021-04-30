5457092d7d206262a41c0c0bba070f19d3887e07
Erbil <erbil.silik@getir.com>
2021-04-30 12:19:05 +0300

    Refactor canvas methods

--- app/canvas/canvas.js
+++ app/canvas/canvas.js
@@ -3,8 +3,6 @@ const {
   DEFAULT_VERTICAL_BORDER,
 } = require('../constants/defaults');
 
-const floodFill = require('./floodFill');
-
 class Canvas {
   constructor() {
     this.width = DEFAULT_WIDTH;
@@ -13,16 +11,25 @@ class Canvas {
     this.markColor = DEFAULT_MARK_COLOR;
   }
 
-  drawCanvas() {
+  draw(callback) {
     for (let i = 0; i < this.height; i++) {
+      const row = i + 1;
       if (!this.image[i]) {
         this.image.push([]);
       }
       for (let j = 0; j < this.width; j++) {
+        const col = j + 1;
         const value = this.image[i][j];
-        this.pushEmpty(value, i, j);
+        callback({ i, j, row, col, value });
       }
     }
+  }
+
+  drawCanvas() {
+    const alg = ({ i, j, colValue }) => {
+      this.pushEmpty(colValue, i, j);
+    };
+    this.draw(alg);
     this.update();
   }
 
@@ -30,93 +37,95 @@ class Canvas {
     const slopeX = x2 - x1;
     const slopeY = y2 - y1;
 
-    for (let i = 0; i < this.height; i++) {
-      if (!this.image[i]) {
-        this.image.push([]);
-      }
-      for (let j = 0; j < this.width; j++) {
-        const value = this.image[i][j];
-        // line
-        if (slopeX === 0 || slopeY === 0) {
-          // vertical line
-          if (slopeX === 0 && (j + 1 >= x1 && j + 1 <= x2) && (i + 1 >= y1 && i + 1 <= y2)) {
-            if (value === undefined) {
-              this.image[i].push(this.markColor);
-            } else {
-              this.image[i][j] = this.markColor;
-            }
-          }
-          // horizontal line
-          else if (slopeY === 0 && (i + 1 === y1 && j + 1 >= x1 && j + 1 <= x2)) {
-            if (value === undefined) {
-              this.image[i].push(this.markColor);
-            } else {
-              this.image[i][j] = this.markColor;
-            }
-          } else {
-            if (value === undefined) {
-              this.image[i].push();
-            } else {
-              this.image[i][j] = value;
-            }
-          }
-        }
-        // empty
-        else {
-          this.pushEmpty(value, i, j);
-        }
-      }
-    }
-    this.update();
-  }
-
-  drawRectangle(x1, y1, x2, y2) {
-    for (let i = 0; i < this.height; i++) {
-      if (!this.image[i]) {
-        this.image.push([]);
-      }
-      for (let j = 0; j < this.width; j++) {
-        const value = this.image[i][j];
-        // rectangle
-        if (
-          (j + 1 >= x1 && j + 1 <= x2) &&
-          (i + 1 === y1 || i + 1 === y2 || (i + 1 === y2 - y1 && (j + 1 === x1 || j + 1 === x2)))
-        ) {
+    const algorithm = ({ i, j, row, col, value }) => {
+      // line
+      if (slopeX === 0 || slopeY === 0) {
+        // vertical line
+        if (slopeX === 0 && (col >= x1 && col <= x2) && (row >= y1 && row <= y2)) {
           if (value === undefined) {
             this.image[i].push(this.markColor);
           } else {
             this.image[i][j] = this.markColor;
           }
         }
-        // empty
-        else {
-          this.pushEmpty(value, i, j);
+        // horizontal line
+        else if (slopeY === 0 && (row === y1 && col >= x1 && col <= x2)) {
+          if (value === undefined) {
+            this.image[i].push(this.markColor);
+          } else {
+            this.image[i][j] = this.markColor;
+          }
+        } else {
+          if (value === undefined) {
+            this.image[i].push();
+          } else {
+            this.image[i][j] = value;
+          }
         }
       }
-    }
+      // empty
+      else {
+        this.pushEmpty(value, i, j);
+      }
+    };
+    this.draw(algorithm);
+    this.update();
+  }
+
+  drawRectangle(x1, y1, x2, y2) {
+    const algorithm = ({ i, j, row, col, value }) => {
+      // rectangle
+      if (
+        (col >= x1 && col <= x2) &&
+        (row === y1 || row === y2 || (row === y2 - y1 && (col === x1 || col === x2)))
+      ) {
+        if (value === undefined) {
+          this.image[i].push(this.markColor);
+        } else {
+          this.image[i][j] = this.markColor;
+        }
+      }
+      // empty
+      else {
+        this.pushEmpty(value, i, j);
+      }
+    };
+    this.draw(algorithm);
+    console.log(this.image);
     this.update();
   }
 
   bucketFill(x, y) {
-    for (let i = 0; i < this.height; i++) {
-      if (!this.image[i]) {
-        this.image.push([]);
-      }
-      for (let j = 0; j < this.width; j++) {
-        const value = this.image[i][j];
+    const algorithm = ({ i, j, row, col }) => {
+      if (x && y) {
+        if (col === x && row === y) {
+          const floodFillUtil = (screen, width, height, x, y, prevC, newC) => {
+            if (x < 0 || x >= height || y < 0 || y >= width || screen[x][y] !== prevC || screen[x][y] === newC) {
+              return;
+            }
 
-        // fill
-        if (x && y) {
-          if (j + 1 === x && i + 1 === y) {
-            floodFill(this.image, this.width, this.height, y, x, this.markColor);
-          }
-        }
-        // empty
-        else {
-          this.pushEmpty(value, i, j);
+            if (screen[x][y] === DEFAULT_EMPTY_COLOR) {
+              screen[x][y] = newC;
+            }
+
+            floodFillUtil(screen, width, height, x + 1, y, prevC, newC);
+            floodFillUtil(screen, width, height,x - 1, y, prevC, newC);
+            floodFillUtil(screen, width, height, x, y + 1, prevC, newC);
+            floodFillUtil(screen, width, height, x, y - 1, prevC, newC);
+          };
+          const floodFill = (screen, width, height, x, y, newC) => {
+            const prevC = screen[x][y];
+            floodFillUtil(screen, width, height, x, y, prevC, newC);
+          };
+          floodFill(this.image, this.width, this.height, y, x, this.markColor);
         }
       }
-    }
+      // empty
+      else {
+        this.pushEmpty(value, i, j);
+      }
+    };
+    this.draw(algorithm);
     this.update();
   }
 
