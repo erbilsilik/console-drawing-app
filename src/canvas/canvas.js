a1b779ab4cd6266bf8e3a5717503df33c007099d
Erbil <erbil.silik@getir.com>
2021-05-01 17:00:31 +0300

    Optimize bucket fill algorithm

--- src/canvas/canvas.js
+++ src/canvas/canvas.js
@@ -48,34 +48,40 @@ class Canvas {
   }
 
   bucketFill(x, y) {
-    const algorithm = (row, col) => {
-      if (x && y) {
-        if (col === x && row === y) {
-          const floodFillUtil = (screen, width, height, x, y, prevC, newC) => {
-            if (x < 0 || x >= height || y < 0 || y >= width || screen[x][y] !== prevC || screen[x][y] === newC) {
-              return;
-            }
-
-            if (screen[x][y] === DEFAULT_EMPTY_COLOR) {
-              screen[x][y] = newC;
-            }
-
-            floodFillUtil(screen, width, height, x + 1, y, prevC, newC);
-            floodFillUtil(screen, width, height, x - 1, y, prevC, newC);
-            floodFillUtil(screen, width, height, x, y + 1, prevC, newC);
-            floodFillUtil(screen, width, height, x, y - 1, prevC, newC);
-          };
-          const floodFill = (screen, width, height, x, y, newC) => {
-            const prevC = screen[x][y];
-            floodFillUtil(screen, width, height, x, y, prevC, newC);
-          };
-          floodFill(this.image, this.width, this.height, y, x, this.markColor);
-          return true;
-        }
+    const floodFillUtil = (screen, width, height, x, y, prevC, newC) => {
+      if (
+        x < 0 ||
+        x >= height ||
+        y < 0 ||
+        y >= width ||
+        screen[x][y] !== prevC ||
+        screen[x][y] === newC
+      ) {
+        return;
       }
-      return false;
+
+      if (screen[x][y] === DEFAULT_EMPTY_COLOR) {
+        screen[x][y] = newC;
+      }
+
+      floodFillUtil(screen, width, height, x + 1, y, prevC, newC);
+      floodFillUtil(screen, width, height, x - 1, y, prevC, newC);
+      floodFillUtil(screen, width, height, x, y + 1, prevC, newC);
+      floodFillUtil(screen, width, height, x, y - 1, prevC, newC);
     };
-    this[draw](algorithm);
+    const floodFill = (screen, width, height, x, y, newC) => {
+      const prevC = screen[x][y];
+      floodFillUtil(screen, width, height, x, y, prevC, newC);
+    };
+    floodFill(
+      this.image,
+      this.width,
+      this.height,
+      y,
+      x,
+      this.markColor
+    );
+    this[update]();
   }
 
   [draw](callback) {
